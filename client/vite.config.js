44de6cfd549dc6ec78cc732cf1ea304d2d6da4ef
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2024-02-26 16:12:58 -0500

    Removed a build line

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    outDir: 'client',
-    rollupOptions:{
-      input:'src/main.jsx'
-    }
-  },
   plugins: [react()],
     server: {
     port: 3000,
