7188ab6cac3ed0f68262ed5f1d1580511d53b79e
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2024-03-16 22:16:42 -0400

    testing fix

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/items');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/items',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
 module.exports = mongoose.connection;
