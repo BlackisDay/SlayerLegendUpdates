09ca7b3657b94ce83e61bfdb69d7387a5bef9b46
Victor Hernandez <141363547+BlackisDay@users.noreply.github.com>
2024-03-20 16:08:29 -0400

    testing server

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/items',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/items',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
